132029c310d087b0b16359c666c580ff57963e09
limzh <liminzhang1013@gmail.com>
2020-07-14 23:49:13 +0800

    Create suffix-arrray.cpp

---
+++ suffix-arrray.cpp
@@ -0,0 +1,17 @@
+/*suffix-array algorithm is to 
+store and sort suffix-array of a string in O(nlog^2(n))*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+    string s;
+    cin >> t;
+    s += "$";
+    int n = s.size()
+    vector<int> p(n), c(n);
+    {
+        // k = 0
+        vector<pair<char, int>>
+    }
+    return 0;
+}
