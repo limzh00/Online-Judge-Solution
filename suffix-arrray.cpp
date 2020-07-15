ff0e0c24955227e1cbf5cc1d0d6f601943f3d822
limzh <liminzhang1013@gmail.com>
2020-07-15 20:36:37 +0800

    create solution

--- suffix-arrray.cpp
+++ suffix-arrray.cpp
@@ -11,7 +11,20 @@ int main(void){
     vector<int> p(n), c(n);
     {
         // k = 0
-        vector<pair<char, int>>
+        vector<pair<char, int>> a(n);
+        for (size_t i = 0; i < n; i++)
+        {
+            a[i] = {s[i], i};
+        }
+
+        sort(a.begin(), a.end());
+
+        for (size_t i = 1; i < n; i++)
+        {
+            if (a[i].first == a[i - 1].first)
+        }
+        
+        
     }
     return 0;
 }
