a2d02dac2b25cab56ae07dd72e0b8b819c942ad8
limzh <liminzhang1013@gmail.com>
2020-07-26 00:52:49 +0800

    3.cpp

--- LeetCode/3.cpp
+++ LeetCode/3.cpp
@@ -1,6 +1,6 @@
 # include<iostream>
 # include<vector>
-# include<stdio.h>
+# include<string>
 using namespace std;
 
 class Solution {
@@ -10,11 +10,22 @@ public:
 
 int
 Solution::lengthOfLongestSubstring(string s){
-    
+    int res = 1; int j = 0;
+    if(s.size() == 0) return 0;
+    if(s.size() == 1) return 1;
+    for(int i = 1; i < s.size(); i++){
+        int tmp = j;
+        while(s[i] != s[j] && i != j) j++; 
+        j = (i == j) ? tmp : j+1;
+        res = (res > i - j + 1) ? res : i - j + 1;
+    }
+    return res;
 }
 
-
 int main(void){
-    
+    string str = "dsfasdfadaser";
+    Solution sol;
+    int res = sol.lengthOfLongestSubstring(str);
+    cout << res << endl;
     return 0;
 }
