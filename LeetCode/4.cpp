3604ebf5ce504c87b7ffb9a640dc6cf02262a4f6
limzh <liminzhang1013@gmail.com>
2020-07-26 01:47:03 +0800

    Update 4.cpp

--- LeetCode/4.cpp
+++ LeetCode/4.cpp
@@ -1,10 +1,15 @@
 # include<iostream>
-# include<math.h>
+# include<vector>
 using namespace std;
 
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size() + nums2.size();
+        int k = n / 2; // 找第k小的数字(如果n是奇数，那么k刚好是中位数的前一个；如果n是偶数，那么k刚好是中位数的第一个)
+        int index = k/2 - 1;// 去比较index为一半的数字
+        
+        
         
     }
 };
